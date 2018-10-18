1339b28a4d70f009c2b6fd1825da7910b6a1cc59
Will Kirkconnell <wekirkco@mtu.edu>
2018-10-18 11:40:34 -0400

    Created dependencies for Jacoco and comfirmed to work when compiling Maven project

---
+++ src/test/java/Test.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test {
+
+    @org.junit.Test
+    public void test1() {
+        Driver sys = new Driver();
+        assertEquals("TeamSoftwareProject [C:\\Users\\Will\\IdeaProjects\\Team-Software-Project] - ...\\src\\main\\java\\Driver.java [TeamSoftwareProject] - IntelliJ IDEA", sys.getTitle());
+
+    }
+}
